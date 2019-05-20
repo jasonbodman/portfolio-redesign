a57414f1e62de9eee4cf9237e5fa243094311e03
Jason Bodman <bodmand1@gmail.com>
2019-05-19 20:07:29 -0400

    updated post.js file to show header

--- blog/web/pages/post.js
+++ blog/web/pages/post.js
@@ -1,14 +1,16 @@
+import client from '../client'
+
 function Post(props) {
-    return (
-      <article>
-        <h1>{props.slug}</h1>
-      </article>
-    )
-  }
-  
-  Post.getInitialProps = async function(context) {
-    const { slug } = context.query
-    return { slug }
-  }
-  
-  export default Post
+  return (
+    <article>
+      <h1>{props.title}</h1>
+    </article>
+  )
+}
+
+Post.getInitialProps = async function(context) {
+  const { slug } = context.query
+  return await client.fetch('*[_type == "post" && slug.current == $slug][0]', { slug })
+}
+
+export default Post
